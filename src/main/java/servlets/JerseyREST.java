607a779e52a3e23ff71d8cfa2344b34d7f200db8
pwang13 <pwang13@ncsu.edu>
2016-09-23 15:03:55 -0400

    er

--- src/main/java/servlets/JerseyREST.java
+++ src/main/java/servlets/JerseyREST.java
@@ -1,22 +1,4 @@
-package main.java.servlets;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import com.google.gson.Gson;
-
-import main.models.Todo;
 
 @Path("/users/{id}/todos/{todo}")	  
 public class JerseyREST 
