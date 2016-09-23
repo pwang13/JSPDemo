93a987dfc23c7091e8f8aaf5ff8b30550ca4395a
pwang13 <pwang13@ncsu.edu>
2016-09-23 14:48:26 -0400

    error

--- src/main/java/servlets/RESTDemo.java
+++ src/main/java/servlets/RESTDemo.java
@@ -1,16 +1,4 @@
-package main.java.servlets;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.google.gson.Gson;
 
 @WebServlet(urlPatterns="/api/users/*")
 public class RESTDemo extends HttpServlet 
